95b20971380853d9896b4536833b3048bb532ea1
Yamuna Kadava <yamunak139@gmail.com>
2024-01-05 01:30:56 +0530

    Checkpoint 3 of ToDo List App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 
 export default function App() {
+  const ToDoItem=["Read SpringBoot","Complete assignments","Prepare breakfast","Sleep for 2 hours","Take a shower"];
   return (
     <div className="Application">
       <header className="header">
@@ -8,7 +9,13 @@ export default function App() {
       </header>
 
       <ul>
-        <li><span>Nothing to do buddy. Sleep!!</span></li>
+        {ToDoItem.map((data,ids) => {
+          return(
+          <li key={ids}>
+            <span>{data}</span>
+          </li>
+          );
+        })}
       </ul>
     </div>
   );
